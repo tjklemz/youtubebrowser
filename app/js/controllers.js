62f3ac68c782346aac7b007c0393690e901627b3
Thomas Klemz <thomas.klemz@creoagency.com>
2013-12-03 18:50:39 -0800

    youtube browser

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,20 +2,78 @@
 
 /* Controllers */
 
-var phonecatApp = angular.module('phonecatApp', []);
+var app = angular.module('app', ['phonecatFilters', 'ngAnimate']);
 
-phonecatApp.controller('PhoneListCtrl', function($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.',
-     'age': 1},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.',
-     'age': 2},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.',
-     'age': 3}
-  ];
+function makeYouTubeURL(userID, startIndex) {
+  return "https://gdata.youtube.com/feeds/api/users/" + 
+          userID + 
+          "/uploads?alt=json&max-results=50&start-index=" + 
+          startIndex;
+}
 
-  $scope.orderProp = 'age';
+function makeYouTubePreviewURL(video) {
+  var id = video.id.$t.split('/').reverse()[0];
+  return "http://img.youtube.com/vi/" + id + "/maxresdefault.jpg";
+}
+
+function parseVideos(videos) {
+  for(var i = 0; i < videos.length; ++i) {
+    var video = videos[i];
+    video.previewImg = makeYouTubePreviewURL(video);
+  }
+  return videos;
+}
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var scrollAmt = $(window).scrollTop();
+    var height = $("#videos").height();
+    
+    if(scrollAmt >= 0.7*height) {
+      var $scope = angular.element($('body')).scope();
+      $scope.$apply(function() {
+        $scope.next();
+      });
+    }
+  });
+});
+
+app.controller('PhoneListCtrl', function($scope) {
+  $scope.findVideos = function(start) {
+    $scope.start = start ? start : 1;
+    if (!start) { 
+      $scope.videos = [];
+      $scope.doneChunking = false;
+    }
+
+    if ($scope.userID) {
+      $.ajax({
+        'url': makeYouTubeURL($scope.userID, $scope.start),
+        'dataType': "json",
+        'success': function(data) {
+          console.log('spam');
+          if(data.feed.entry && data.feed.entry.length > 0) {
+            var videos = parseVideos(data.feed.entry);
+            $scope.$apply(function() {
+              $scope.videos = $scope.videos.concat(videos);
+            });
+            $scope.start += $scope.videos.length;
+          }
+          if (data.feed.entry) {
+            $scope.doneChunking = !data.feed.entry.length;
+          }
+        },
+        'error': function() {
+          $scope.videos = [];
+        }
+      });
+    }
+
+    $scope.next = function() {
+      if(!$scope.doneChunking) {
+        $scope.doneChunking = true;
+        $scope.findVideos($scope.start);
+      }
+    }
+  }
 });
--- app/js/filters.js
+++ app/js/filters.js
@@ -1,3 +1,35 @@
 'use strict';
 
+function hashRipper(hash) {
+	var arr = []
+	if (typeof hash !== 'object') {
+		arr.push(hash.toString());
+		return arr;
+	}
+	Object.keys(hash).forEach(function(key) {
+		var val = hash[key];
+		arr = arr.concat(hashRipper(val));
+	});
+	return arr;
+}
+
 /* Filters */
+angular.module('phonecatFilters', []).filter('ripper', function() {
+  return function(items, filter) {
+      if (!filter) {
+          return items;
+      }  
+      var result = {};
+      angular.forEach(items, function(item, itemKey) {
+      	var arr = hashRipper(item);
+      	for(var i = 0; i < arr.length; ++i) {
+      		var fieldVal = arr[i];
+          	if (fieldVal && fieldVal.toLowerCase().indexOf(filter.toLowerCase()) > -1){
+				result[itemKey] = item;
+			}
+		}
+      });
+      console.log('done');
+      return result;
+    };
+});
